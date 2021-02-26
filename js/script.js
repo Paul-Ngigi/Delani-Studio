66630894e8e59b0840680014dd8f9a8a7b652755
Paul-Ngigi <paulkush7777@gmail.com>
2021-02-26 13:57:17 +0300

    add toggle to the what to section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
+    $(".clickable").click(function() {
+        $(".design-hidden").toggle();
+        $(".design-visible").toggle();
+    });
+    $(".dev-clickable").click(function() {
+        $(".development-hidden").toggle();
+        $(".development-visible").toggle();
+    });
+    $(".management-clickable").click(function() {
+        $(".management-hidden").toggle();
+        $(".management-visible").toggle();
+    });
 })
