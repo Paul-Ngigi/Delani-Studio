e73cc81d5f8400a8f83012d4f4a47761e1811320
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-01 07:09:14 +0300

    add a hover effect on the portfolio images

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-$(document).ready(function(){
-    $(".clickable").click(function() {
+$(document).ready(function () {
+    $(".clickable").click(function () {
         $(".design-hidden").toggle();
         $(".design-visible").toggle();
     });
-    $(".dev-clickable").click(function() {
+    $(".dev-clickable").click(function () {
         $(".development-hidden").toggle();
         $(".development-visible").toggle();
     });
-    $(".management-clickable").click(function() {
+    $(".management-clickable").click(function () {
         $(".management-hidden").toggle();
         $(".management-visible").toggle();
+    });
+    $("#portfolio img").hover(function () {
+        $(this).stop().animate({
+            opacity: .5 
+        });
+        }, function () {
+            $(this).stop().animate({
+                opacity: 2
+            });
     });
 })
